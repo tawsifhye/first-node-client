2ff720ea68e1222b0968c49468145b60905a18bc
tawsifhye <tawsif.lucse@gmail.com>
2021-10-21 16:03:03 +0600

    Use fetch to load data from server, middleware, handle cors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[users, setUsers] = useState([])
+    useEffect(()=>{
+      fetch("http://localhost:5000/users")
+      .then(res => res.json())
+      .then(data => setUsers(data))
+    },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="">
+      <ul>
+        {
+          users.map(user =>
+            <li key={user.id}>{user.name}</li>
+          )
+        }
+      </ul>
     </div>
   );
 }
